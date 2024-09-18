927cf4630d82dd06e88e91ba7905a74eb29b5d9b
Matthias Vitt <matthias.vitt@gmail.com>
2024-09-18 13:55:14 +0000

    added ssh and sensor

--- airflowdev/dag1.py
+++ airflowdev/dag1.py
@@ -16,7 +16,7 @@ from airflow.sensors.date_time import DateTimeSensor
 
 
 
-@task(task_id="print_the_context")
+@task(task_id="print_the_context", trigger_rule="all_success") 
 def print_context(ds,execution_date, **kwargs):
     """Print the Airflow context and ds variable from the context."""
     pprint(kwargs)
@@ -34,24 +34,46 @@ def print_context(ds,execution_date, **kwargs):
     print(f"task now: {datetime.now()}")
     return "Whatever you return gets printed in the logs {{execution_date}}"
 
-@task(task_id="delayed_task")
-def run_delayed(execution_date):
-    #trigger_date = execution_date + datetime.timedelta(minutes=3)
-    print(f"task now: {datetime.now()}")
+# @task(task_id="delayed_task")
+# def run_delayed(execution_date):
+#     #trigger_date = execution_date + datetime.timedelta(minutes=3)
+#     print(f"task now: {datetime.now()}")
     
     
 
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.sftp.sensors.sftp import SFTPSensor
 
 
 
 with DAG(dag_id="mv1",schedule=None,start_date=datetime(2024,1,1),catchup=False,tags=["examplemv"]) as dag:
     run_this = print_context()
     run_other = print_context()
+
+    ssh_task = SSHOperator(
+    task_id='ssh_task',
+    ssh_conn_id='ssh2aws',
+    command='/home/cloud_user/bin/job_success'
+    )
+    
+    ssh_task_error = SSHOperator(
+    task_id='ssh_task_error',
+    ssh_conn_id='ssh2aws',
+    command='/home/cloud_user/bin/job_error'
+    )
+
+
+    sftp_with_operator = SFTPSensor(task_id="sftp_operator", path="/home/cloud_user/bin/touchfile", poke_interval=10, timeout=30,sftp_conn_id="sftp2aws")
     
     #execution_date.tomorrow()
     #DateTimeSniffer
     #sensor=DateTimeSensor(task_id="datetime_sensor",target_time="{{ execution_date.replace(minute=45) }}",mode="poke")
     
-    run_this >>  run_other
+    
+    all_start = [run_this, ssh_task, ssh_task_error]
+    sftp_with_operator >> all_start
+    
+    
+    all_start >> run_other
     
     
